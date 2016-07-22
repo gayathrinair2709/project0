8d1978866faec675994521e430224caef11939d9
gayathrinair2709 <gayathrinair702@gmail.com>
2016-07-22 23:24:32 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,9 @@
+public class DataConversion
+{public static void main(String args[]){
+byte a=120;
+short b=a;
+System.out.println("the value is "+b);
+short c=560;
+byte d=(byte)c;
+System.out.println("the value is "+d);
+}}
